ea0dd3a206400cab9654392f8929efa11af8c411
冬祉 <ninan.nn@alibaba-inc.com>
2025-12-24 16:52:23 +0800

    test: test jvm 8

--- tests/java/build.gradle.kts
+++ tests/java/build.gradle.kts
@@ -23,8 +23,9 @@ group = "com.alibaba.opensandbox"
 version = "1.0.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 repositories {
