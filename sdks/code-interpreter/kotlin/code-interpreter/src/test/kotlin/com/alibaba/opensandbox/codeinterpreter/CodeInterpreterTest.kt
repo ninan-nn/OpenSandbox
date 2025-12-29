5f403b639b68d8dcbb36e8584f82e704c10e30d1
冬祉 <ninan.nn@alibaba-inc.com>
2025-12-29 10:56:43 +0800

    refactor(sdk): remove unnecessary delegate in code interpreter

--- sdks/code-interpreter/kotlin/code-interpreter/src/test/kotlin/com/alibaba/opensandbox/codeinterpreter/CodeInterpreterTest.kt
+++ sdks/code-interpreter/kotlin/code-interpreter/src/test/kotlin/com/alibaba/opensandbox/codeinterpreter/CodeInterpreterTest.kt
@@ -18,26 +18,19 @@ package com.alibaba.opensandbox.codeinterpreter
 
 import com.alibaba.opensandbox.codeinterpreter.domain.services.Codes
 import com.alibaba.opensandbox.sandbox.Sandbox
-import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.SandboxEndpoint
-import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.SandboxInfo
-import com.alibaba.opensandbox.sandbox.domain.models.sandboxes.SandboxMetrics
 import com.alibaba.opensandbox.sandbox.domain.services.Commands
 import com.alibaba.opensandbox.sandbox.domain.services.Filesystem
 import com.alibaba.opensandbox.sandbox.domain.services.Metrics
-import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
-import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertSame
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import java.time.Duration
 import java.util.UUID
 
 @ExtendWith(MockKExtension::class)
@@ -97,69 +90,5 @@ class CodeInterpreterTest {
     @Test
     fun `codes should return code service`() {
         assertSame(codeService, codeInterpreter.codes())
-    }
-
-    @Test
-    fun `getEndpoint should delegate to sandbox`() {
-        val port = 8888
-        val endpoint = mockk<SandboxEndpoint>()
-        every { sandbox.getEndpoint(port) } returns endpoint
-
-        assertSame(endpoint, codeInterpreter.getEndpoint(port))
-        verify { sandbox.getEndpoint(port) }
-    }
-
-    @Test
-    fun `getInfo should delegate to sandbox`() {
-        val info = mockk<SandboxInfo>()
-        every { sandbox.getInfo() } returns info
-
-        assertSame(info, codeInterpreter.getInfo())
-        verify { sandbox.getInfo() }
-    }
-
-    @Test
-    fun `getMetrics should delegate to sandbox`() {
-        val metrics = mockk<SandboxMetrics>()
-        every { sandbox.getMetrics() } returns metrics
-
-        assertSame(metrics, codeInterpreter.getMetrics())
-        verify { sandbox.getMetrics() }
-    }
-
-    @Test
-    fun `renew should delegate to sandbox`() {
-        val timeout = Duration.ofMinutes(10)
-        every { sandbox.renew(timeout) } just Runs
-
-        codeInterpreter.renew(timeout)
-
-        verify { sandbox.renew(timeout) }
-    }
-
-    @Test
-    fun `pause should delegate to sandbox`() {
-        every { sandbox.pause() } just Runs
-
-        codeInterpreter.pause()
-
-        verify { sandbox.pause() }
-    }
-
-    @Test
-    fun `kill should delegate to sandbox`() {
-        every { sandbox.kill() } just Runs
-
-        codeInterpreter.kill()
-
-        verify { sandbox.kill() }
-    }
-
-    @Test
-    fun `isHealthy should delegate to sandbox`() {
-        every { sandbox.isHealthy() } returns true
-
-        assertTrue(codeInterpreter.isHealthy())
-        verify { sandbox.isHealthy() }
     }
 }
